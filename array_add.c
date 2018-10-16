13ba79cbc5dce88599d62253afcd89861557fb3a
Terran Liu <38232187+TianrunLiu@users.noreply.github.com>
2018-10-16 11:43:42 -0400

    Update and rename array_add.c to lib_add.c

--- array_add.c
+++
@@ -1,4 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "arraylib.h"
-#include "listlib.h"
---
+++ lib_add.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "musiclib.h"
+#include "listlib.h"
+
+struct song_node * lib_add(struct song_node *table, char[100] name, char[100] artist){
+  return 0;
+}
