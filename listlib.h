c09e258a91395ec75a1bcfdbbd7cbc9600cfdfe3
Terran Liu <38232187+TianrunLiu@users.noreply.github.com>
2018-10-16 11:33:52 -0400

    Update listlib.h

--- listlib.h
+++ listlib.h
@@ -1,1 +1,13 @@
-
+#ifndef LIST_H
+#define LIST_H
+struct song_node{char name[100]; char artist[100]; struct song_node *next;};
+struct song_node * table[27];
+void list_print(struct song_node *list);
+struct song_node * list_insert_front(struct song_node *list, char[100] name, char[100] artist);
+struct song_node * list_free(struct song_node *list);
+struct song_node * list_insert_order(struct song_node *list, char[100] name, char[100] artist);
+struct song_node * list_first_song(struct song_node *list, char[100] artist);
+struct song_node * list_remove(struct song_node *list, char[100] name, char[100] artist);
+struct song_node * list_random(struct song_node *list);
+struct song_node * list_search(struct song_node *list, char[100] song, char[100] artist);
+#endif
