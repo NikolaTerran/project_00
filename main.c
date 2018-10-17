3b986cf222d39bc2cd21a80e84fc8b0852ab0320
Terran Liu <38232187+TianrunLiu@users.noreply.github.com>
2018-10-17 11:43:53 -0400

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 
 int main(){
 
+printf("******Initializing Testing Environment******\n");
+printf("******Initializing Music Library************\n");
+
 printf("LINKED LIST TESTS\n====================================\n\n");
 
 printf("Testing list_insert_front:\n");
@@ -41,7 +44,42 @@ printf("====================================\n\n");
 
 printf("MUSIC LIBRARY TESTS\n====================================\n\n");
 
-printf("Testing 
-  return 0;
+printf("Testing lib_add:\n");
+
+printf("====================================\n\n");
+
+printf("Testing lib_search:\n");
+
+printf("====================================\n\n");
+
+printf("Testing lib_search_artist:\n");
+
+printf("====================================\n\n");
+
+printf("Testing lib_print_letter:\n");
+
+printf("====================================\n\n");
+
+printf("Testing lib_print_artist:\n");
+
+printf("====================================\n\n");
+
+printf("Testing lib_print:\n");
+
+printf("====================================\n\n");
+
+printf("Testing lib_shuffle:\n");
+
+printf("====================================\n\n");
+
+printf("Testing lib_remove:\n");
+
+printf("====================================\n\n");
+
+printf("Testing lib_free:\n");
+
+printf("====================================\n\n");
+  
+return 0;
 
 }
